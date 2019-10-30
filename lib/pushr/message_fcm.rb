f409db8304283a8e1aaf68f5cc418e94c241ca59
Tom Pesman <tom@9to5.software>
2019-10-30 10:37:17 +0100

    add fcm_options field to message

--- lib/pushr/message_fcm.rb
+++ lib/pushr/message_fcm.rb
@@ -2,7 +2,7 @@ module Pushr
   class MessageFcm < Pushr::Message
     POSTFIX = 'fcm'.freeze
 
-    attr_accessor :name, :data, :notification, :android, :webpush, :apns, :token, :topic, :condition
+    attr_accessor :name, :data, :notification, :android, :webpush, :apns, :fcm_options, :token, :topic, :condition
 
 
     def to_message
@@ -15,7 +15,7 @@ module Pushr
 
     def to_hash
       hsh = { type: self.class.to_s, app: app, name: name, data: data, notification: notification, android: android,
-              webpush: webpush, apns: apns, token: token, topic: topic, condition: condition }
+              webpush: webpush, apns: apns, fcm_options: fcm_options, token: token, topic: topic, condition: condition }
       hsh[Pushr::Core.external_id_tag] = external_id if external_id
       hsh
     end
